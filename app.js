8ba52c6db97733f191032d64d1c03d228ca6e785
tuanngoc0298@gmail.com <75868787+tuanngoc0298@users.noreply.github.com>
2022-09-18 13:06:49 +0700

    Fix Performance

--- app.js
+++ app.js
@@ -485,7 +485,7 @@ const apps = {
             return `
                 <li class="playlist_item"  data-index = "${index} ">
                     <div class = "playlist_item-img">
-                        <img src="${song.image}" alt="">
+                        <img src="${song.image}" alt="" loading="lazy">
                     </div>
                     <div class="playlist_item-info">
                         <h3 class="playlist_item-name text">${song.name}</h3>
